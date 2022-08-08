491669120ec24b2a16080d9630098279185a1afe
Jahongir <jahongir22@gmail.com>
2022-08-08 13:42:27 +0500

    j1

---
+++ script.js
@@ -0,0 +1,18 @@
+var amout = prompt('ismingizni kiriting')
+var amout2 = +prompt('yoshingizni kiriting')
+alert('ismingiz: ' + amout + ' yoshingiz: ' +amout2 + ' sizga misol beramiz rozi bosangiz OK bosing')
+
+var plus = +prompt ('6 + 4=')
+var minus = +prompt ('12 - 3=')
+var kopay = +prompt ('4 * 3=')
+var boluv = +prompt ('12 / 3=')
+var qoldiq = +prompt ('7 % 2=')
+alert('javoblar uchun raxmat togriligni blsh uchun console bolimiga oting')
+
+
+console.info('ism ' + amout , ' yosh: ' + amout2)
+console.info('togri javob 6 + 4= 10 , sizning javob: ' + plus)
+console.info('togri javob 12 - 3= 9 , sizning javob: ' + minus)
+console.info('togri javob 4 * 3= 12 , sizning javob: ' + kopay)
+console.info('togri javob 12 / 3= 4 , sizning javob: ' + boluv)
+console.info('togri javob 7 % 2= 1 , sizning javob: ' + qoldiq)
